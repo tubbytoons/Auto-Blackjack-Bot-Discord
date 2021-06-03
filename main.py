9bf37cef3323f60fa27b94fb97e97f1eee8be64b
T-T <85304158+tubbytoons@users.noreply.github.com>
2021-06-03 11:54:54 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import requests
+import time
+title = """
+This Is 100% free Source will be on github
+found at github.com/tubbytoons
+ 
+ Black Jack Auto spin
+"""
+time.sleep(1)
+print(title)
+time.sleep(1)
+token = str(input("Input your token: "))
+channelID = str(input("Input your Black Jack channel ID: "))
+
+
+
+def sendMessage(token, channel_id, message):
+    url = 'https://discord.com/api/v8/channels/{}/messages'.format(channel_id)
+    data = {"content": message}
+    header = {"authorization": token}
+
+    r = requests.post(url, data=data, headers=header)
+    
+while True:
+    time.sleep(305)
+    print("Message Sent.")
+    sendMessage(token, channelID, "+spin")
